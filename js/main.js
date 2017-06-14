9984d5147149b85c47b0c8174bc8a2583fecb642
liuyuchen <181484908@qq.com>
2017-06-14 14:24:33 +0800

    add load jquery with require

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-something.innerText =  'hello world!'
-console.log('1')
+require(['jquery.min'],function(jquery){
+    console.log('1')
+    let $body = $('body')
+    let $hello = $('<div><div>')
+    $hello.text('Hello World with jQuery').appendTo($body)
+})
+something.innerText = 'Hello World with DOM'
